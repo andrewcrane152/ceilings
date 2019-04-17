5bfac6dac704b5fcfa3e04fa90bf3f6623058441
Andrew Crane <andrew.crane@3-form.com>
2019-04-17 16:39:12 -0600

    fix: repairs notification service for snackbars

--- src/app/_services/alert.service.ts
+++ src/app/_services/alert.service.ts
@@ -1,19 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { MatSnackBarConfig, MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class AlertService {
   private subject = new Subject<any>();
+  snackBarConfig: MatSnackBarConfig;
 
-  constructor() {}
+  constructor(
+    public snackBar: MatSnackBar,
+    private zone: NgZone
+  ) { }
 
-  success(message: string) {
-    this.subject.next({ type: 'success', text: message });
+  public error(message, action = 'Dismiss', duration = 5000) {
+    this.snackBarConfig = new MatSnackBarConfig();
+    this.snackBarConfig.duration = duration;
+    this.zone.run(() => {
+      this.snackBar.open(message, action, this.snackBarConfig);
+    })
   }
 
-  error(message: string) {
-    this.subject.next({ type: 'error', text: message });
+  public success(message, action = 'Dismiss', duration = 5000) {
+    this.snackBarConfig = new MatSnackBarConfig();
+    this.snackBarConfig.duration = duration;
+    this.zone.run(() => {
+      this.snackBar.open(message, action, this.snackBarConfig);
+    })
   }
+
 
   getMessage(): Observable<any> {
     return this.subject.asObservable();
